239ae86acf597e34c090226467d163d801638d49
marek-zelc-gd <41433191+marek-zelc-gd@users.noreply.github.com>
2023-09-07 10:33:08 +0200

    Update tap.py

--- tap_hubspot/tap.py
+++ tap_hubspot/tap.py
@@ -114,7 +114,6 @@ class TapHubSpot(Tap):
         ),
     ).to_dict()
 
-
     def discover_streams(self) -> List[Stream]:
         """Return a list of discovered streams."""
         return [stream_class(tap=self) for stream_class in STREAM_TYPES]
